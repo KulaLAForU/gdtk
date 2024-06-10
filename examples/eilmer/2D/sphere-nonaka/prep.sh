1accab8181f2ca10130020ec9287090395b28be1
Jianshu Wu <40930186+KulaLAForU@users.noreply.github.com>
2024-06-10 17:03:15 +0900

    Add Park's 2T model to calculate K_eq
    Original from the Park's 2T model, using A1 - A5 parameters to calculate the K_eq.

--- examples/eilmer/2D/sphere-nonaka/prep.sh
+++ examples/eilmer/2D/sphere-nonaka/prep.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 prep-gas air-5sp-2T.inp air-5sp-2T.gas
-prep-chem air-5sp-2T.gas GuptaEtAl-air-2T.lua air-5sp-6r-2T.chem
+# prep-chem air-5sp-2T.gas GuptaEtAl-air-2T.lua air-5sp-6r-2T.chem
+cp ./Park-2T/air-5sp-6r-2T.chem .
 prep-kinetics air-5sp-2T.gas air-5sp-6r-2T.chem air-VT-energy-exchange.lua air-VT.exch
 e4shared --job=nonaka --prep
