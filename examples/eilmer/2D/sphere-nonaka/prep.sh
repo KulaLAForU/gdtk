b9892601c58f39ffe74ff5136de7585a6ceff92b
Jianshu Wu <40930186+KulaLAForU@users.noreply.github.com>
2024-07-17 12:55:36 +0900

    Update prep.sh for 1993 Park's
    Park C. Review of chemical-kinetic problems of future NASA missions. I - Earth entries. Journal of Thermophysics and Heat Transfer [Internet]. 1993 Jul [cited 2024 Jun 10];7(3):385–98. Available from: https://arc.aiaa.org/doi/10.2514/3.431

--- examples/eilmer/2D/sphere-nonaka/prep.sh
+++ examples/eilmer/2D/sphere-nonaka/prep.sh
@@ -2,5 +2,6 @@
 prep-gas air-5sp-2T.inp air-5sp-2T.gas
 # prep-chem air-5sp-2T.gas GuptaEtAl-air-2T.lua air-5sp-6r-2T.chem
 cp ./Park-2T/air-5sp-6r-2T.chem .
-prep-kinetics air-5sp-2T.gas air-5sp-6r-2T.chem air-VT-energy-exchange.lua air-VT.exch
+# prep-kinetics air-5sp-2T.gas air-5sp-6r-2T.chem air-VT-energy-exchange.lua air-VT.exch
+cp ./Park-2T/air-VT.exch .
 e4shared --job=nonaka --prep
