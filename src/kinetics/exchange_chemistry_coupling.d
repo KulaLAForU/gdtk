63fe4e04536cc6d63ff6e2e32b5d58f80c252caf
Forrest Wu <kulaladn@gmail.com>
2024-09-21 15:04:58 +0900

    bug fix

--- src/kinetics/exchange_chemistry_coupling.d
+++ src/kinetics/exchange_chemistry_coupling.d
@@ -155,12 +155,14 @@ class PreferentialDissociation : ExchangeChemistryCoupling {
     
     @nogc
     number Gvanish(in GasState gs) {
-        return 0.3 * D;
+        double L0 = 0.3 * D;
+        return to!number(L0);
     }
 
     @nogc
     number Gappear(in GasState gs) {
-        return 0.3 * D;
+        double L0 = 0.3 * D;
+        return to!number(L0);
     }
 
 private:
@@ -199,13 +201,15 @@ class KimHTCEnergyLoss : ExchangeChemistryCoupling {
     @nogc
     number Gvanish(in GasState gs) {
         number T = gs.T;
-        return D * exp(K1 / T + K2 + K3 * log(T) + K4 * T + K5 * T * T);
+        double L0 = D * exp(K1 / T + K2 + K3 * log(T) + K4 * T + K5 * T * T);
+        return to!number(L0);
     }
 
     @nogc
     number Gappear(in GasState gs) {
         number T = gs.T;
-        return D * exp(K1 / T + K2 + K3 * log(T) + K4 * T + K5 * T * T);
+        double L0 = D * exp(K1 / T + K2 + K3 * log(T) + K4 * T + K5 * T * T);
+        return to!number(L0);
     }
 
 private:
