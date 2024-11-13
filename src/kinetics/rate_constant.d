7a94e7e1053a48563c5fb07c7fdb1b1b7f8a330b
Jianshu Wu <40930186+KulaLAForU@users.noreply.github.com>
2024-11-13 14:53:55 +0900

    Update rate_constant.d

--- src/kinetics/rate_constant.d
+++ src/kinetics/rate_constant.d
@@ -153,7 +153,7 @@ public:
     override number eval(in GasState Q)
     {
         number T = Q.T;
-        return _A*pow(T, _n)*exp(-_C/T) / (exp(_A_1*(T/10000.0) + _A_2 + _A_3*log(10000.0/T) + _A_4*(10000.0/T) + _A_5 * pow(T / 10000.0, 2)));
+        return _A*pow(T, _n)*exp(-_C/T) / (exp(_A_1*(T/10000.0) + _A_2 + _A_3*log(10000.0/T) + _A_4*(10000.0/T) + _A_5 * pow(10000.0 / T, 2)));
     }
 
 private:
